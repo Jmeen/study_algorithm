ce6416d4e853fae769cbff75b53d5201c5cb2f33
Jaemin.Lim <jmeeen@gamil.com>
2021-08-07 14:51:10 +0900

    leetcode 344. Reverse String

--- Algorithm_Study/src/com/beginner/leetcode.java
+++ Algorithm_Study/src/com/beginner/leetcode.java
@@ -1,17 +1,34 @@
 package com.beginner;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class leetcode {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		System.out.println(isPalindrome(s));
+//		char s = sc.next().charAt(0);
+		char[] s = { 'h', 'e', 'l', 'l', 'o' };
+		reverseString(s);
 
 	}
 
+	public static void reverseString(char[] s) {
+		// 344. Reverse String
+		int i = 0;
+		int x = s.length - 1;
+		while (i <= x) {
+			char tempcha = s[i];
+			s[i] = s[x];
+			s[x] = tempcha;
+			x--;
+			i++;
+		}
+		System.out.println(s);
+	}
+
 	public static boolean isPalindrome(String s) {
+		// 125. Valid Palindrome
 		// 한글유니코드(\uAC00-\uD7A3), 숫자 0~9(0-9), 영어 소문자a~z(a-z), 대문자A~Z(A-Z)를 제외한(^) 단어일
 		// 경우 체크 / 공백(\s)
 		String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
