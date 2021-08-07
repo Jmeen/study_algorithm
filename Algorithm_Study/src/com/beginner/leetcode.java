dba0812f58119ca8b8b9ba30ba62089e88a9d30d
Jaemin.Lim <jmeeen@gamil.com>
2021-08-07 15:38:18 +0900

    leetcode #1. TWOSUM

--- Algorithm_Study/src/com/beginner/leetcode.java
+++ Algorithm_Study/src/com/beginner/leetcode.java
@@ -8,8 +8,31 @@ public class leetcode {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 //		char s = sc.next().charAt(0);
-		char[] s = { 'h', 'e', 'l', 'l', 'o' };
-		reverseString(s);
+		int[] nums = { 2, 5, 5, 11 };
+		int target = 10;
+
+		System.out.println(Arrays.toString(twoSum(nums, target)));
+
+	}
+
+	public static int[] twoSum(int[] nums, int target) {
+		// leetcode 1. Two Sum
+		int[] result = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 1; j < nums.length; j++) {
+				if (i == j) {
+					continue;
+				}
+				if (nums[i] + nums[j] == target) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+
+		return result;
 
 	}
 
